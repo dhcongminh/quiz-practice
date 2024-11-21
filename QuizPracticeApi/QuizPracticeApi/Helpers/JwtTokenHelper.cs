c64de3cc140390e9646876fa54b893fd3bf606f5
dhcongminh <dhcongminh@gmail.com>
2024-11-21 12:33:26 +0700

    [API] JWT Token Helper class

---
+++ QuizPracticeApi/QuizPracticeApi/Helpers/JwtTokenHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using QuizPracticeApi.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace QuizPracticeApi.Helpers {
+    public class JwtTokenHelper {
+        public static string GenerateToken(User user, IConfiguration configuration) {
+            var jwtSettings = configuration.GetSection("Jwt");
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken (
+                issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(double.Parse(jwtSettings["ExpireMinutes"])),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
